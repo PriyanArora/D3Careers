87dc5c200e466078dd7e58667ffa8c37993d334b
Priyan Arora <Priyanarora22@gmail.com>
2026-04-01 23:30:36 -0300

    feat(alumni): add profiles, soft gate, and webhook signature verification

--- client/src/pages/AlumniPage.jsx
+++ client/src/pages/AlumniPage.jsx
@@ -1,3 +1,33 @@
+import api from "../api"
+import { useState, useEffect } from "react"
+import {Link} from "react-router-dom"
+
 export default function AlumniPage() {
-  return <h1>Alumni</h1>
+  const [alumni, setAlumni] = useState([])
+  const [error, setError] = useState(null)
+  
+  useEffect(()=>{
+    async function getAlumni(){
+      try{
+        const res = await api.get("/api/alumni")
+        setAlumni(res.data.alumni)
+      }
+      catch(error){
+        setError("Alumni couldn't get fetched")
+      }
+    }
+    getAlumni()
+  }, [])
+
+  return(
+    <div>
+    {alumni.map(person =>(  //person is a variable name, could be anything, and every map item needs key prop, using person._id
+      <div key = {person._id}> 
+        <Link to = {`/alumni/${person._id}`}>
+          <p>{person.name}</p>
+        </Link>
+      </div>
+    ))}
+    </div>
+  )
 }
